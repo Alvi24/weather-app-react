f6592dedffcfe689bd278a1017489b9e6543dc91
Alvi24 <24alvi@gmail.com>
2023-01-16 21:11:18 +0100

    main

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export default class NavBar extends Component {
+  
+  render() {
+    return (
+      <nav>
+        <FontAwesomeIcon icon="faHouse" /><p>Aisha</p>
+      </nav>
+    )
+  }
+}
